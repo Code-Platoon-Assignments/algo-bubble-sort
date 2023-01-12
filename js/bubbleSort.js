161d47c5823548b8749c8ff4b93f4b3d752c3129
fravila08 <fr4v1l4@gmail.com>
2023-01-12 09:56:44 -0800

    s

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,7 +1,23 @@
-var sequence = [4, 3, 5, 0, 1]
-var swaps = 0
-
 // Your Code Here
+const bubbleSort =(arr, swp=0)=>{
+    let start= swp;
+    let swaps=swp;
+    for(let idx=0; idx<arr.length -1; idx++){
+        if (arr[idx]>arr[idx+1]){
+            holder=arr[idx+1]
+            arr[idx+1]=arr[idx]
+            arr[idx]=holder
+            swaps++
+        }
+    }
+    if (swaps ===start){
+        console.log("Final result: " + arr)
+        console.log("Swaps: " + swaps)
+    }
+    else{
+        bubbleSort(arr, swaps)
+    }
+}
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+var sequence = [4, 3, 5, 0, 1]
+bubbleSort(sequence)
--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,19 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
-
 # Your Code Here
+def bubble_sort(lst, swps=0):
+    start=swps
+    swaps= swps
+    for idx in range(len(lst)-1):
+        if lst[idx]>lst[idx+1]:
+            holder=lst[idx+1]
+            lst[idx+1]=lst[idx]
+            lst[idx]=holder
+            swaps+=1
+    if swaps == start:
+        print(f"Final result: {lst}")
+        print(f"Swaps: {swaps}")
+    else:
+        bubble_sort(lst, swaps)
+        
+sequence = [4, 3, 5, 0, 1]        
+bubble_sort(sequence)
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
